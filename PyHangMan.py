105520154849d620343b1966b6f498dc017c48e7
AnthonyTaylor <AnTaylor@Northamptonshire.gov.uk>
2020-06-17 21:02:58 +0100

    adding exit message on keyboard interupt

--- PyHangMan.py
+++ PyHangMan.py
@@ -1,7 +1,7 @@
-import json
 import random
 
 def hard_mode():
+    import json
     with open("Data/words_dictionary.json", "r") as word_file:
         word_list = json.load(word_file)
     secret = random.choice(list(word_list))
@@ -16,35 +16,43 @@ def easy_mode():
 
 
 def print_output(guess, secret):
-    print(guess + " in in the word")
+    print(guess.upper() + " in in the word")
 
 
-secret = []
-game_mode = input("Easy (e), or hard (h)? ")
-if game_mode == 'e':
-    secret = easy_mode()
-elif game_mode == 'h':
-    secret = hard_mode()
+def game():
+    secret = []
+    game_mode = input("Easy (e), or hard (h)? ").lower()
+    if game_mode == 'e':
+        secret = easy_mode()
+    elif game_mode == 'h':
+        secret = hard_mode()
 
-word_length = len(secret)
-print("There are " + str(word_length) + " letters")
-print (secret)
+    word_length = len(secret)
+    print("There are " + str(word_length) + " letters")
+    print (secret)
 
-keep_going = True
-lives = 1
+    keep_going = True
+    lives = 1
 
-while keep_going:
-    guess = input("enter a letter or guess the whole word: ")
-    if list(guess) == secret:
-        keep_going = False
-        print("YOU WIN")
-    elif len(guess) == 1 and guess in secret:
-        print_output(guess, secret)
-    else:
-        lives -= 1
-        if lives == 0:
-            print("you lose!")
-            print("The word was "+ "".join(secret).upper())
+    while keep_going:
+        guess = input("enter a letter or guess the whole word: ").lower()
+        if list(guess) == secret:
             keep_going = False
+            print("YOU WIN")
+        elif len(guess) == 1 and guess in secret:
+            print_output(guess, secret)
         else:
-            print("incorrect, lose a life")
+            lives -= 1
+            if lives == 0:
+                print("you lose!")
+                print("The word was "+ "".join(secret).upper())
+                keep_going = False
+            else:
+                print("incorrect, lose a life")
+
+
+try:
+    game()    
+except KeyboardInterrupt:
+    print("\n\nexiting ...")
+    SystemExit
